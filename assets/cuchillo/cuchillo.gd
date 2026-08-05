a10a6bd1ea68ca2a8b8782208d9cd97a072d7bee
H0rus25 <miguelejfg2003@gmail.com>
2026-08-05 18:38:56 -0400

    Se le había agregado al script de ambas armas la logica del buff, pero se elimino luego de no haber comunicacion con el encargado de los items

--- assets/cuchillo/cuchillo.gd
+++ assets/cuchillo/cuchillo.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 #definimos el daño que hará el arma
-#Esta variable aparece en el inspector de Godot y está como 5.0 su valor
+#Esta variable aparece en el inspector de Godot y está como 7.6 su valor
 @export var damage = 1.0
 
 #Referencia directa al nodo hijo anim (AnimationPlayer)
@@ -10,6 +10,9 @@ extends Node3D
 @onready var Sonido_cuchillo = $Sonido_cuchillo
 #Referencia al nuevo nodo Area3D para recoger el arma del suelo
 @onready var pickup_area = $PickupArea
+
+# Almacena quién es el dueño actual del arma para evitar auto-daño en PvP
+var portador: Node3D = null
 
 #Para determinar si el jugador ataca o no
 var can_attack = false
@@ -25,7 +28,7 @@ func _ready() -> void:
 
 #Esta funcion se encarga de revisar constantemente si se quiere atacar
 func _process(_delta: float) -> void:
-	#Detecta si se presiona el boton de atacar o shoot (click izquierdo(
+	#Detecta si se presiona el boton de atacar o shoot (click izquierdo)
 	if Input.is_action_pressed("shoot") and can_attack and not anim.is_playing():
 		anim.play("Golpear")
 		Sonido_cuchillo.play()
@@ -37,7 +40,8 @@ func _process(_delta: float) -> void:
 #Estas son funciones para detectar la colision entre el arma y los personajes
 func _on_hitbox_body_entered(body: Node3D) -> void:
 	#Reemplazar "player" por el nombre del nodo real de los personajes, le puse ese nombre de forma provisional
-	if body.is_in_group("player") and  not enemies_in_range.has(body):
+	#Detecta a cualquier jugador, PERO ignora al que sostiene el arma (portador)
+	if body.is_in_group("player") and body != portador and not enemies_in_range.has(body):
 		enemies_in_range.append(body)
 
 func _on_hitbox_body_exited(body: Node3D) -> void:
@@ -48,11 +52,13 @@ func _on_hitbox_body_exited(body: Node3D) -> void:
 func _on_pickup_area_body_entered(body: Node3D) -> void:
 	# Comprobamos si es el jugador quien entra al área de recogida
 	if body.is_in_group("player"):
-		# Busca el nodo de la mano del jugador para acoplar el arma.
-		# Quien programe el personaje del jugador solo debe cambiar "Mano" por el nombre real de su nodo de agarre.
-		var mano_jugador = body.get_node_or_null("Mano")
+		# Busca el nodo de la mano del jugador usando la ruta exacta de la cámara
+		var mano_jugador = body.get_node_or_null("Head/Camera3D/Hand")
 		
 		if mano_jugador:
+			# Guardamos en la memoria del arma quién es su dueño para el sistema PvP
+			portador = body
+			
 			# Desengancha el arma de su posición en el suelo y la vuelve hija de la mano del jugador
 			reparent(mano_jugador)
 			
